bf26cc6d24ccda0d104ee91f0c69f9ea985f54d0
minhaz-arch <im.minhaz.2001@gmail.com>
2025-07-03 13:05:07 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,16 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
+kernel.compiler=
+kernel.made=
+kernel.version=
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -29,10 +28,10 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-IS_SLOT_DEVICE=0;
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=auto
 RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+PATCH_VBMETA_FLAG=auto
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
